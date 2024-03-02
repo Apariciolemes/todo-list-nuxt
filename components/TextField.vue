15765bd70c448b3e668542003237ebe7bd30d5e6
Aparicio Lemes Neto <aparicio.neto@mxctech.com.br>
2024-03-01 23:07:12 -0300

    feat: create component TextField with TDD

--- components/TextField.vue
+++ components/TextField.vue
@@ -1,4 +1,22 @@
 <template>
-
-    <div class="">TEXT-FIELD HERE</div>
+  <input :value="props.value" @input="setInputValue($event)">
 </template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+const emit = defineEmits(['emit-input'])
+const props = defineProps({
+    value: {
+        type: String,
+        default: ''
+    }
+})
+
+
+const setInputValue = (event: InputEvent) => {
+    const value = (event.target as HTMLInputElement).value
+    emit('emit-input', value)
+}
+
+</script>
--- tests/TextField.spec.ts
+++ tests/TextField.spec.ts
@@ -7,4 +7,27 @@ describe('TextField', () => {
         const wrapper = mount(TextField)
         expect(wrapper.vm).toBeTruthy()
     })
+
+    it('render with props value', () => {
+        const wrapper = mount(TextField, {
+            props: {
+                value: 'Task 1'
+
+            }
+        })
+        const input = wrapper.find('input')
+        expect(input.element.value).toEqual('Task 1')
+    })
+
+    it('should output the update input value with emit', () => {
+        const wrapper = mount(TextField)
+        const input = wrapper.find('input')
+        input.setValue('Task 1')
+
+        const updateEvent = wrapper.emitted('emit-input')
+        const valueEmitted = updateEvent ? updateEvent[0][0] : ''
+
+        expect(input.element.value).toEqual('Task 1')
+        expect(valueEmitted).toEqual('Task 1')
+    })
 })
